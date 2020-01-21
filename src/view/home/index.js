90d2d5c0020ee31594edf8bd1d02eef71fe1443a
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-21 16:28:00 +0100

    added necessary component for the view

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,7 +1,46 @@
 import React from "react";
-import './index.scss'
+import "./index.scss";
+import { Header, Searchbar } from "../../component";
+import { Title, Text, InputText, Icon } from "../../component/styled-component";
+import search from "../../img/icons/search.png";
+import plus from "../../img/icons/plus.png";
+import Button from "../../component/Button";
+import { Link } from "react-router-dom";
+
+const Right = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">Sign In</a>
+      </li>
+      <li>
+        <a href="/">Sign Up</a>
+      </li>
+      <li className="no-visible">
+        <a href="/">Add Slang</a>
+      </li>
+    </ul>
+  );
+};
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header right={<Right />} />
+      <main className="landing-page">
+        <div className="container">
+          <Title>9ja Slangs</Title>
+          <Text subtitle small align="center">
+            Consequat veniam occaecat ad tempor quis nostrud nostrud magna.
+          </Text>
+          <Searchbar />
+          <Link to="/result/1">To Result</Link>
+          <Button fab customStyle="fab-btn">
+            <Icon src={plus} alt="plus" />
+          </Button>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
