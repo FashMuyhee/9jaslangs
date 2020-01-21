724a120a14551a8b92c74b2756f83d3d8fa8c82b
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-21 16:25:45 +0100

    restructured app layout

--- src/view/index.js
+++ src/view/index.js
@@ -5,22 +5,25 @@ import Result from "./result";
 import SignIn from "./sign-in";
 import SignUp from "./sign-up";
 import Eroor404 from "./404";
-
-const Layout = () => (
-  <div className="layout">
-    <header>My Header</header>
-    <main>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/result/:slang" component={Result} />
-        <Route component={Eroor404} />
-        <Redirect to="/" />
-      </Switch>
-    </main>
-    <footer>my footer</footer>
-  </div>
-);
-
+const Layout = () => {
+  return (
+    <>
+        <Switch>
+          {/* <Route path="/" exact component={Result} /> */}
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/result/:slang" component={Result} />
+          <Route component={Eroor404} />
+          <Redirect to="/" />
+        </Switch>
+      <footer>
+        Built & Designed by
+        <a href="/" target="blank">
+          Fash Muyhee
+        </a>
+      </footer>
+    </>
+  );
+};
 export default Layout;
