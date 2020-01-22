0266b580dfa75f37c8df7aa52b71e026c53c7d7f
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-22 03:53:01 +0100

    initial design after initial commit

--- src/view/sign-in/index.js
+++ src/view/sign-in/index.js
@@ -1,7 +1,59 @@
 import React from "react";
-import './index.scss'
+import "./index.scss";
+import { Button } from "../../component";
+import {
+  InputText,
+  Title,
+  Text,
+  Icon
+} from "../../component/styled-component";
+import colors from "../../helpers/colors";
+import welcome from "../../img/welcome.png";
+import facebook from "../../img/icons/facebook.png";
+import google from "../../img/icons/google.png";
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <main className="signin-page">
+      <div className="content">
+        <div className="greeting">
+          <img src={welcome} alt="welcome" />
+          <Title case="capitilize">Welcome Back !</Title>
+          <Text subtitle>Sign in to your existing account of 9ja Slangs</Text>
+        </div>
+        <form className="signin-form">
+          <InputText
+            borderColor={colors.accent}
+            name="email"
+            type="email"
+            placeholder="Email"
+          />
+          <InputText
+            borderColor={colors.accent}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <Button type="submit" customStyle="btn">
+            Sign In
+          </Button>
+        </form>
+        <Text className="small-text">or connect using</Text>
+        <div className="social-button">
+          <Button iconOnly customStyle="btn-facebook">
+            <Icon src={facebook} alt="facebook" />
+          </Button>
+          <Button iconOnly customStyle="btn-google">
+            <Icon src={google} alt="google" />
+          </Button>
+        </div>
+        <Text className="link-text">
+          Don't have an account ? <Link to="/signup">Sign Up</Link>
+        </Text>
+      </div>
+    </main>
+  );
 };
 
 export default SignIn;
