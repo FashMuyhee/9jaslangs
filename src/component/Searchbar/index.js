82a6e78b0b0ee9e6c2c49b2ba20df48095e33a71
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-22 03:46:46 +0100

    added new props

--- src/component/Searchbar/index.js
+++ src/component/Searchbar/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./index.scss";
 import { InputText, Icon } from "../styled-component";
 import search from "../../img/icons/search.png";
-const Searchbar = ({ customClass }) => {
+const Searchbar = ({ customClass='' }) => {
   return (
     <div className={`searchbar ${customClass}`}>
       <InputText
         name="query"
         placeholder="Lorem quis amet "
         className="search-input"
+        borderColor="#79d2f2"
       />
       <Icon
         src={search}
