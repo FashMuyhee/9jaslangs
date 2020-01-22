cef3c73b8a1895f21496d0d017a4126cfef3d63a
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-22 03:44:40 +0100

    added new props

--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./index.scss";
-const Button = ({ fab, iconOnly, withIcon, children, customStyle }) => {
-  let buttonStyle;
+const Button = ({
+  fab,
+  iconOnly,
+  withIcon,
+  children,
+  customStyle='',
+  onClick,
+  link
+}) => {
+  let buttonStyle = "just-button";
   if (iconOnly) buttonStyle = "icon-only";
+  else if (link) buttonStyle = "link";
   else if (withIcon) buttonStyle = "with-icon";
   else if (fab) buttonStyle = "fab";
 
   return (
-    <button className={` ${customStyle} ${buttonStyle}`} type="button">
+    <button
+      className={` ${customStyle} ${buttonStyle}`}
+      type="button"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
