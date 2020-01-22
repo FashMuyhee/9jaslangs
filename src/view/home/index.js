12a4b4f7d9e0ae6e70fcc2a092b98e1041b296b2
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-22 03:50:48 +0100

    added modal functionality

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
-import { Header, Searchbar } from "../../component";
-import { Title, Text, InputText, Icon } from "../../component/styled-component";
-import search from "../../img/icons/search.png";
+import { Header, Searchbar, Modal, Button } from "../../component";
+import {
+  Title,
+  Text,
+  InputText,
+  Icon,
+  TextArea
+} from "../../component/styled-component";
 import plus from "../../img/icons/plus.png";
-import Button from "../../component/Button";
 import { Link } from "react-router-dom";
 
-const Right = () => {
+const Right = ({ onClick }) => {
   return (
     <ul>
       <li>
-        <a href="/">Sign In</a>
+        <Link to="/signin">Sign In</Link>
       </li>
       <li>
-        <a href="/">Sign Up</a>
+        <Link to="/signup">Sign Up</Link>
       </li>
-      <li className="no-visible">
-        <a href="/">Add Slang</a>
+      <li className="no-visible" onClick={onClick}>
+        <Button>Add Slang</Button>
       </li>
     </ul>
   );
 };
 const Home = () => {
+  const [modal_state, setModalState] = useState(false);
+
+  const handleModalClose = () => {
+    setModalState(false);
+  };
+
+  const handleModalOpen = () => {
+    setModalState(true);
+    setModalState(true);
+  };
   return (
     <>
-      <Header right={<Right />} />
+      <Header right={<Right onClick={handleModalOpen} />} />
       <main className="landing-page">
         <div className="container">
           <Title>9ja Slangs</Title>
@@ -34,10 +48,34 @@ const Home = () => {
           </Text>
           <Searchbar />
           <Link to="/result/1">To Result</Link>
-          <Button fab customStyle="fab-btn">
+          <Button fab customStyle="fab-btn" onClick={handleModalOpen}>
             <Icon src={plus} alt="plus" />
           </Button>
         </div>
+        <Modal
+          show={modal_state}
+          title="New Slang"
+          handleClose={handleModalClose}
+        >
+          <form className="add-form">
+            <InputText
+              placeholder="Mad oo"
+              name="slang"
+              borderColor="#79d2f2"
+            />
+            <TextArea
+              borderColor="#79d2f2"
+              name="meaning"
+              placeholder="A way of expressing suprise to an happening"
+            />
+            <TextArea
+              borderColor="#79d2f2"
+              name="examples"
+              placeholder="E.g Man-u beat Arsenal 4-0; Mad oo"
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Modal>
       </main>
     </>
   );
