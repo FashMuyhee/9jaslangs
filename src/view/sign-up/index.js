40c68cc9c76d2da31b3dee075b07f66194b7aa69
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-22 03:53:10 +0100

    initial design after initial commit

--- src/view/sign-up/index.js
+++ src/view/sign-up/index.js
@@ -1,7 +1,57 @@
 import React from "react";
-import './index.scss'
+import "./index.scss";
+import { Button } from "../../component";
+import { InputText, Title, Text } from "../../component/styled-component";
+import colors from "../../helpers/colors";
+import welcome from "../../img/welcome.png";
+
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <main className="signup-page">
+      <div className="content">
+        <div className="greeting">
+          <img src={welcome} alt="welcome" />
+          <Title case="capitilize">Let Get Started !</Title>
+          <Text subtitle>Create an account with 9ja Slangs to get all features</Text>
+        </div>
+        <form className="signup-form">
+          <InputText
+            borderColor={colors.accent}
+            name="name"
+            type="text"
+            placeholder="Fullname"
+          />
+          <InputText
+            borderColor={colors.accent}
+            name="email"
+            type="email"
+            placeholder="Email"
+          />
+          <InputText
+            borderColor={colors.accent}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <InputText
+            borderColor={colors.accent}
+            name="comfirm-password"
+            type="password"
+            placeholder="Confirm Password"
+          />
+          <Button type="submit" customStyle="btn">
+            Sign Up
+          </Button>
+        </form>
+
+        <Text className="link-text">
+          Have an account already ? <Link to="/signin">Sign In</Link>
+        </Text>
+      </div>
+    </main>
+  );
 };
 
 export default SignUp;
