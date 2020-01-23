9a71737f3f51433f3ac4fe5acff10c9b97db5482
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-23 18:34:29 +0100

    initial commit

---
+++ src/component/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './index.scss'
+const Footer = () => {
+  return (
+    <footer>
+      Built & Designed by
+      <a href="/" target="blank">
+        Fash Muyhee
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
