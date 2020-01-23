2c731e8825a662d41e3bf6a1e7e17214e8b19e44
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-23 18:44:54 +0100

    restructure design

--- src/view/sign-in/index.js
+++ src/view/sign-in/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import "./index.scss";
 import { Button } from "../../component";
-import {
-  InputText,
-  Title,
-  Text,
-  Icon
-} from "../../component/styled-component";
+import { InputText, Title, Text, Icon } from "../../component/styled-component";
 import colors from "../../helpers/colors";
 import welcome from "../../img/welcome.png";
 import facebook from "../../img/icons/facebook.png";
@@ -19,38 +14,42 @@ const SignIn = () => {
       <div className="content">
         <div className="greeting">
           <img src={welcome} alt="welcome" />
+        </div>
+        <div className="signin-form">
           <Title case="capitilize">Welcome Back !</Title>
-          <Text subtitle>Sign in to your existing account of 9ja Slangs</Text>
+          <Text subtitle className="welcome-text">
+            Sign in to your existing account of 9ja Slangs
+          </Text>
+          <form>
+            <InputText
+              borderColor={colors.accent}
+              name="email"
+              type="email"
+              placeholder="Email"
+            />
+            <InputText
+              borderColor={colors.accent}
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+            <Button type="submit" customStyle="btn">
+              Sign In
+            </Button>
+          </form>
+          <Text className="small-text">or connect using</Text>
+          <div className="social-button">
+            <Button iconOnly customStyle="btn-facebook">
+              <Icon src={facebook} alt="facebook" />
+            </Button>
+            <Button iconOnly customStyle="btn-google">
+              <Icon src={google} alt="google" />
+            </Button>
+          </div>
+          <Text className="link-text">
+            Don't have an account ? <Link to="/signup">Sign Up</Link>
+          </Text>
         </div>
-        <form className="signin-form">
-          <InputText
-            borderColor={colors.accent}
-            name="email"
-            type="email"
-            placeholder="Email"
-          />
-          <InputText
-            borderColor={colors.accent}
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-          <Button type="submit" customStyle="btn">
-            Sign In
-          </Button>
-        </form>
-        <Text className="small-text">or connect using</Text>
-        <div className="social-button">
-          <Button iconOnly customStyle="btn-facebook">
-            <Icon src={facebook} alt="facebook" />
-          </Button>
-          <Button iconOnly customStyle="btn-google">
-            <Icon src={google} alt="google" />
-          </Button>
-        </div>
-        <Text className="link-text">
-          Don't have an account ? <Link to="/signup">Sign Up</Link>
-        </Text>
       </div>
     </main>
   );
