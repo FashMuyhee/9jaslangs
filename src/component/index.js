1b4d6b3aec32c115575df38edaa294240c043860
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-23 18:41:35 +0100

    imported Footer component

--- src/component/index.js
+++ src/component/index.js
@@ -2,4 +2,5 @@ import Header from "./Header";
 import Button from "./Button";
 import Searchbar from "./Searchbar";
 import Modal from "./Modal";
-export { Header, Button, Searchbar, Modal };
+import Footer from "./Footer";
+export { Header, Button, Searchbar, Modal,Footer };
