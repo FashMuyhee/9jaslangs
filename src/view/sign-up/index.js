168fdf437619d688e3c8d50ea1b251c7b2a2f2ce
fashmuyhee <fashmuyidavid@gmail.com>
2020-01-29 22:30:20 +0100

    added firebase function for signing up and onchange text function

--- src/view/sign-up/index.js
+++ src/view/sign-up/index.js
@@ -1,14 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import { Button } from "../../component";
-import { InputText, Title, Text } from "../../component/styled-component";
+import {
+  InputText,
+  Title,
+  Text,
+  ErrorLabel
+} from "../../component/styled-component";
 import colors from "../../helpers/colors";
+import { validateEmail } from "../../helpers/validators";
 import welcome from "../../img/welcome.png";
+import { Link, Redirect } from "react-router-dom";
 
-import { Link } from "react-router-dom";
+import withFirebaseAuth from "react-with-firebase-auth";
+import firebaseApp from "../../config/firebase";
+import "firebase/firestore";
 
-const SignUp = () => {
-  return (
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const firebaseAppAuth = firebaseApp.auth();
+const Alert = withReactContent(Swal);
+
+const SignUp = ({ createUserWithEmailAndPassword, error, user }) => {
+  const [state, setState] = useState({
+    email: "",
+    name: "",
+    password: "",
+    c_password: "",
+    e_msg: "",
+    p_msg: "",
+    cp_msg: "",
+    loading: false
+  });
+
+  const handleInputChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSignUp = e => {
+    e.preventDefault();
+
+    setState({ ...state, loading: true });
+    const { email, name, password, c_password } = state;
+    // validation
+    if (validateEmail(email)) {
+      if (password && password.length >= 8) {
+        if (c_password && c_password === password) {
+          console.log(state);
+          createUserWithEmailAndPassword(email, password)
+            .then(resp => {
+              firebaseApp
+                .firestore()
+                .collection(`users`)
+                .doc(resp.user.uid)
+                .set({ name, email })
+                .then(data => {
+                  setState({ ...state, loading: false });
+                  Alert.fire({
+                    position: "center",
+                    icon: "success",
+                    title: error,
+                    showConfirmButton: false,
+                    timer: 2000
+                  });
+                });
+            })
+            .catch(error => {
+              setState({ ...state, loading: false });
+
+              /* Alert.fire({
+                position: "center",
+                icon: "error",
+                title: error,
+                showConfirmButton: false,
+                timer: 2000
+              }); */
+              console.error(error);
+            });
+        } else {
+          setState({
+            ...state,
+            loading: false,
+            p_msg: "Passwords are not Equal "
+          });
+        }
+      } else {
+        setState({ ...state, p_msg: "Password is less than 8" });
+      }
+    }
+  };
+  return user ? (
+    <Redirect to="/" />
+  ) : (
     <main className="signup-page">
       <div className="content">
         <div className="greeting">
@@ -19,33 +103,49 @@ const SignUp = () => {
           <Text subtitle className="welcome-text">
             Create an account with 9ja Slangs to get all features
           </Text>
-          <form>
-            <InputText
-              borderColor={colors.accent}
-              name="name"
-              type="text"
-              placeholder="Fullname"
-            />
-            <InputText
-              borderColor={colors.accent}
-              name="email"
-              type="email"
-              placeholder="Email"
-            />
-            <InputText
-              borderColor={colors.accent}
-              name="password"
-              type="password"
-              placeholder="Password"
-            />
-            <InputText
-              borderColor={colors.accent}
-              name="comfirm-password"
-              type="password"
-              placeholder="Confirm Password"
-            />
+          <form onSubmit={handleSignUp}>
+            <div>
+              <InputText
+                borderColor={colors.accent}
+                name="name"
+                type="text"
+                placeholder="Fullname"
+                onChange={handleInputChange}
+              />
+              <ErrorLabel></ErrorLabel>
+            </div>
+            <div className="input-group">
+              <InputText
+                borderColor={colors.accent}
+                name="email"
+                type="email"
+                placeholder="Email"
+                onChange={handleInputChange}
+              />
+              <ErrorLabel>{state.e_msg}</ErrorLabel>
+            </div>
+            <div className="input-group">
+              <InputText
+                borderColor={colors.accent}
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleInputChange}
+              />
+              <ErrorLabel>{state.p_msg}</ErrorLabel>
+            </div>
+            <div className="input-group">
+              <InputText
+                borderColor={colors.accent}
+                name="c_password"
+                type="password"
+                placeholder="Confirm Password"
+                onChange={handleInputChange}
+              />
+              <ErrorLabel>{state.cp_msg}</ErrorLabel>
+            </div>
             <Button type="submit" customStyle="btn">
-              Sign Up
+              {state.loading ? "Creating Account" : "Sign Up"}
             </Button>
           </form>
 
@@ -58,4 +158,6 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default withFirebaseAuth({
+  firebaseAppAuth
+})(SignUp);
